5ff16b11f928fe9a4c22f5ef7a07a0fc2e70972e
sura503 <sisiyabu135@gmail.com>
2025-05-27 16:03:53 +0300

    db connection editied

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,6 @@
 <?php
 // Connect to MySQL
-$conn = new mysqli("sql210.infinityfree.com", "if0_39076130", "poTqIex4vrzpc", "if0_39076130_tertibega_coffee");
+$conn = new mysqli("localhost", "root", "", "tertibega_coffee");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -8,3 +8,4 @@ if ($conn->connect_error) {
 
 
 ?>
+ 
